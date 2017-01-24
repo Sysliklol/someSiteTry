39eb7da90891058e14897ffc09baffc8791680c7
Sysliklol <vkharko@gmail.com>
2017-01-24 21:50:38 +0200

    division

--- assets/main.js
+++ assets/main.js
@@ -11,6 +11,7 @@ function hideBlock() {
         block.css('opacity','0.96');
         $('.header-catalog').css('top','50%')
         $('.main-screen').css('padding-top','16%')
+        $('.our-poslugi').css('margin-top','5%');
 
     }
 else {
@@ -20,7 +21,7 @@ else {
         block.css('height','23%');
         block.css('background','');
         $('.header-catalog').css('top','25%')
-
+        $('.our-poslugi').css('margin-top','0');
     }
 
 }
@@ -36,74 +37,28 @@ $(function(){
         }, 1000);
 
     })
-    $('.partners-photo-'+n).click(function(){
-        $('.partners-text-'+n).hide();
-        $(this).removeClass('partners-photo-'+n);
-        n++;
-        if(n>6) n=1;
-        $('.partners-text-'+n).show();
-        console.log(2);
-        $(this).addClass('partners-photo-'+n);
-    })
-    $('.next-screen-3').click(function(){
 
-        $('.partners-photo-'+n).removeClass('col-md-9')
-        $('.partners-photo-'+n).removeClass('col-xs-6')
-        $('.partners-photo-'+n).removeClass('col-sm-8')
-        $('.partners-photo-'+n).addClass('col-md-4')
-        $('.partners-photo-'+n).addClass('col-xs-3')
-        $('.partners-photo-'+n).addClass('col-sm-3')
-        $('.partners-photo-'+n).css('background-size','200% 100%')
-        $('.partners-text-wrap-'+n).removeClass('col-md-3')
-        $('.partners-text-wrap-'+n).removeClass('col-xs-6')
-        $('.partners-text-wrap-'+n).removeClass('col-sm-4')
-        $('.partners-text-wrap-'+n).addClass('col-sm-9')
-        $('.partners-text-wrap-'+n).addClass('col-xs-9')
-        $('.partners-text-wrap-'+n).addClass('col-md-8')
-        $('.partners-text-wrap-'+n).html('<h1> Oikos </h1>' + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod');
-    })
+
 
     $('#contacts').click(function(){
-        hide_all();
-        $('.screen-4').show();
-        $('#contacts').addClass("active");
-        $('.badge').hide();
-        $('.contact-us').show();
-        $('#small-contacts').addClass('active');
-        $('#contacts').append("<div class='border-active-right'>");
+        animate();
+        $('.loading').show();
     });
     $('#main').click(function(){
-        hide_all();
-        $('.mouse-icon').show();
-        $('#main').addClass("active");
-        $('.screen-2').show();
-        $('.screen-3').show();
-        $('.screen-4').show();
-        $('.badge').show();
-        $('.text-on-main-wrap').show();        $('#main').append("<div class='border-active'>");
+        animate();
+        $('.loading').show();
     });
     $('#poslugi').click(function(){
-        hide_all();
-        $('.screen-poslugi').show();
-        $('.our-poslugi').show();
-        $('#poslugi').addClass("active");
-        $('#poslugi').append("<div class='border-active-right'>");
+        animate();
+        $('.loading').show();
     })
     $('#effects').click(function(){
-        hide_all();
-        $('.text-on-main-wrap').show();
-        $('.effects-main').show();
-        $('.effects-samples').show();
-        $('#effects').addClass("active");
-        $('#effects').append("<div class='border-active'>");
+        animate();
+        $('.loading').show();
     })
     $('#catalog').click(function(){
-        hide_all();
-        $('.katalog').show();
-        $('.main-screen').css('height','200vh');
-        $('.cb-slideshow').css('height','100%');
-        $('#catalog').addClass("active");
-        $('#catalog').append("<div class='border-active'>");
+        animate();
+        $('.loading').show();
     })
     $('.catalog-button').click(function(){
         hide_all();
@@ -112,6 +67,10 @@ $(function(){
         $('.main-screen').css('height','60vh');
         $('.header-catalog').text('Каталог продукции > ' + $(this).siblings('.text-sample-text').text())
         $('.cb-slideshow').css('height','60vh');
+    })
+    $('#gallery').click(function(){
+        animate();
+        $('.loading').show();
     })
  function hide_all(){
      $('body').scrollTop(0);
@@ -148,7 +107,7 @@ $(function(){
      function fade(){
          $('.loading').hide();
      }
-     setTimeout(fade,1700);
+     setTimeout(fade,5000);
      fade();
 
  }

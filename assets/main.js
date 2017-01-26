3d448847b51a39a4ab1483acc41322e448661922
Sysliklol <vkharko@gmail.com>
2017-01-26 19:55:38 +0200

    123123

--- assets/main.js
+++ assets/main.js
@@ -2,14 +2,14 @@ var n =1;
 $(window).on("load", function() {
     setTimeout( function() {$('.loading').hide()},2000);
 });
-
+var image_url = "./images_main_page/texture-dark-grey.png"
 function hideBlock() {
     var block = $('.menu-big');
     var menu = $('.menu-small');
     if($('body').scrollTop() >= 200) {
         block.css('position','fixed');
         block.css('top','0');
-        block.css('background','url(../images_main_page/texture-dark-grey.png)');
+        block.css('background','url('+image_url+')');
         block.css('height',$(window).height()*0.15);
         block.css('opacity','0.96');
         $('.header-catalog').css('top','50%')
@@ -48,7 +48,10 @@ $(function(){
         $('.podmenu-header').text('ОбРАЗЦЫ > ' + $(this).siblings('.text-sample-text').text())
         $('.partners-109-hrn').show();
     })
-
+    $('.podmenu-header').click(function(){
+        $('.effects-samples').toggleClass('moveLeft');
+        $('.partners-109-hrn').hide();
+    })
     $('.catalog-button').click(function(){
         hide_all();
         $('.catalog-podmenu').show();
@@ -60,37 +63,37 @@ $(function(){
     $('#gallery').click(function(){
         $('.loading').show();
         $('.loading').hide();
-
+        window.location.href='gallery.html';
 
     })
     $('#catalog').click(function(){
         $('.loading').show();
 
-
+        window.location.href='catalog.html';
 
     })
     $('#contacts').click(function(){
         $('.loading').show();
 
-
+        window.location.href='contacts.html';
 
     })
     $('#effects').click(function(){
         $('.loading').show();
-
+        window.location.href='effects.html';
 
 
     })
 
     $('#main').click(function(){
         $('.loading').show();
-
+        window.location.href='index.html';
 
 
     })
     $('#poslugi').click(function(){
         $('.loading').show();
-
+        window.location.href='poslugi.html';
 
 
     })
